72a5ab92de350043598509f30f8e23324832dcf4
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-07 23:20:16 +0200

    Saving for retirement - solved

---
+++ C#/SavingForRetirement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SavingForRetirement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int Adiff = 0;
+            int Atot = 0;
+
+            string[] subs = input.Split(" ");
+
+            int Btot = (int.Parse(subs[1]) - int.Parse(subs[0])) * int.Parse(subs[2]);
+
+
+            while (Atot <= Btot)
+            {
+                Atot += int.Parse(subs[4]);
+                Adiff++;
+            }
+
+            int Aretire = Adiff + int.Parse(subs[3]);
+            Console.WriteLine(Aretire);
+        }
+    }
+}
