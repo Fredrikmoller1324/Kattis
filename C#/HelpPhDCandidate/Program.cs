a355247535f905203938d56357ac87bfcd5211e7
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-07 21:58:33 +0200

    Help a PhD - solved

---
+++ C#/HelpPhDCandidate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelpPhDCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cases = int.Parse(Console.ReadLine());
+            List<string> inputs = new List<string>();
+
+            for (int i = 0; i < cases; i++)
+            {
+                string input = Console.ReadLine();
+                inputs.Add(input);
+            }
+
+            foreach (var input in inputs)
+            {
+
+                if (char.IsDigit(input[0]))
+                {
+                    string[] fixedString = input.Split('+');
+                    Console.WriteLine(int.Parse(fixedString[0].ToString()) + int.Parse(fixedString[1].ToString()));
+                }
+                else
+                {
+                    Console.WriteLine("skipped");
+                }
+            }
+        }
+    }
+}
