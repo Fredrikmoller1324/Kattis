4fc24313d3cf74b7ff8277f8621cdc4f9cbf801b
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-08 13:43:26 +0200

    Some Sum - Solved

---
+++ C#/SomeSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SomeSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 9:
+                        Console.WriteLine("Either");
+                        break;
+                    case 2:
+                    case 6:
+                    case 10:
+                        Console.WriteLine("Odd");
+                        break;
+                    case 4:
+                    case 8:
+                        Console.WriteLine("Even");
+                        break;
+                }
+            
+            
+        }
+    }
+}
