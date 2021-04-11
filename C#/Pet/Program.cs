05c904fd73e2be33af2b1a3a9a9ecda4c55da402
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-11 17:29:18 +0200

    Pet - Solved

---
+++ C#/Pet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> allFullPoints = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string[] contestantPoints = Console.ReadLine().Split(' ');
+
+                List<int> contestantPointsInts = contestantPoints.Select(s => int.Parse(s)).ToList();
+
+                allFullPoints.Add(contestantPointsInts.Sum());
+            }
+
+            int contenderWinner = allFullPoints.IndexOf(allFullPoints.Max());
+
+            Console.WriteLine($"{contenderWinner} {allFullPoints.Max()}");
+        }
+    }
+}
