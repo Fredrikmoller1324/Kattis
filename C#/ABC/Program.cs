c758bb3d395494c9919a4706bbff0cf95bd4b543
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-12 14:39:58 +0200

    ABC - Solved

---
+++ C#/ABC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] inputIntInOrder = input.Select(number => int.Parse(number)).OrderBy(x => x).ToArray();
+
+            string order = Console.ReadLine();
+
+            switch (order)
+            {
+                case "ABC":
+                    Console.WriteLine($"{inputIntInOrder[0]} {inputIntInOrder[1]} {inputIntInOrder[2]}");
+                    break;
+                case "ACB":
+                    Console.WriteLine($"{inputIntInOrder[0]} {inputIntInOrder[2]} {inputIntInOrder[1]}");
+                    break;
+                case "BAC":
+                    Console.WriteLine($"{inputIntInOrder[1]} {inputIntInOrder[0]} {inputIntInOrder[2]}");
+                    break;
+                case "BCA":
+                    Console.WriteLine($"{inputIntInOrder[1]} {inputIntInOrder[2]} {inputIntInOrder[0]}");
+                    break;
+                case "CAB":
+                    Console.WriteLine($"{inputIntInOrder[2]} {inputIntInOrder[0]} {inputIntInOrder[1]}");
+                    break;
+                case "CBA":
+                    Console.WriteLine($"{inputIntInOrder[2]} {inputIntInOrder[1]} {inputIntInOrder[0]}");
+                    break;
+            }
+        }
+    }
+}
--- C#/Parking/Program.cs
+++ C#/Parking/Program.cs
@@ -11,7 +11,7 @@ namespace Parking
 
             for (int i = 0; i < cases; i++)
             {
-                int amountStoresVisited = int.Parse(Console.ReadLine());
+                int amountStoresVisited = int.Parse(Console.ReadLine()); // onödig men krävs för att få rätt input (variabel används ej)
                 string[] storeAdresses = Console.ReadLine().Split(' ');
 
                 int[] storeAdressAsInt = storeAdresses.Select(adress => int.Parse(adress)).ToArray();
