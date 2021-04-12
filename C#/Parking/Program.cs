c1b4eee1750dc7550077ebe188ab4b30a3d029ac
Fredrik Moller <fredrik.moller1324@gmail.com>
2021-04-12 14:07:40 +0200

    Parking2 - Solved

---
+++ C#/Parking/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cases; i++)
+            {
+                int amountStoresVisited = int.Parse(Console.ReadLine());
+                string[] storeAdresses = Console.ReadLine().Split(' ');
+
+                int[] storeAdressAsInt = storeAdresses.Select(adress => int.Parse(adress)).ToArray();
+
+                Console.WriteLine((storeAdressAsInt.Max() - storeAdressAsInt.Min()) * 2 );
+
+            }
+        }
+    }
+}
