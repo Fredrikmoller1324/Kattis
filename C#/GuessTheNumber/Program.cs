473fedb5da8440cea503b0fbc2be154603992466
Fredrik Möller <fredrik.moller@iths.se>
2022-09-23 17:22:02 +0200

    finished guessthenumber

---
+++ C#/GuessTheNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue = 1;
+            int maxValue = 1000;
+
+            while (true)
+            {
+                var guess = (minValue + maxValue) / 2;
+
+                if(Console.Out != null)
+                    Console.Out.Flush();
+
+                Console.WriteLine(guess);
+                
+                var answer = Console.ReadLine();
+
+                if(answer == "correct")
+                {
+                    return;
+                }
+                else if(answer == "higher")
+                {
+                    minValue = guess + 1;
+                }
+                else if(answer == "lower")
+                {
+                    maxValue = guess + 1;
+                }
+            }
+        }
+    }
+}
