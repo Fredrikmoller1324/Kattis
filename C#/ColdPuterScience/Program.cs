dce28941545c25f9f5ae6d1e8e71a7866594182e
Fredrik Möller <fredrik.moller@iths.se>
2022-09-25 18:53:56 +0200

    finish coldputerscience

---
+++ C#/ColdPuterScience/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ColdPuterScience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalAmountCollectedTemps = int.Parse(Console.ReadLine());
+
+            int amountTempsBelowZero = Console.ReadLine().Split(" ").Where(temp => temp.Contains("-")).Count();
+
+            Console.WriteLine(amountTempsBelowZero);
+        }
+    }
+}
