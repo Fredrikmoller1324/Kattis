8315c4ff02750071ce89dce54960a3ac31c767cf
Fredrik Möller <fredrik.moller@iths.se>
2022-09-26 11:10:43 +0200

    forgot code

--- C#/FuriousCocktail/Program.cs
+++ C#/FuriousCocktail/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FuriousCocktail
 {
@@ -6,7 +8,28 @@ namespace FuriousCocktail
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            string[] potsAndApplyTime = Console.ReadLine().Split(" ");
+
+            int totAmountPots = int.Parse(potsAndApplyTime[0]);
+            int applyTime = int.Parse(potsAndApplyTime[1]);
+
+            Dictionary<int, int> potsWithDurationTime = new Dictionary<int, int>();
+
+            for (int i = 0; i < totAmountPots; i++)
+            {
+                int durationTime = int.Parse(Console.ReadLine());
+
+                potsWithDurationTime.Add(i, durationTime);
+            }
+
+            if (potsWithDurationTime.Values.Max() - (applyTime * (totAmountPots - 1)) > 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        } 
     }
 }
